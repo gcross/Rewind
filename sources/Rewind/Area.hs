733942b53b803a2abeab8cb8ac00edb01dafe187
Gregory Crosswhite <gcrosswhite@gmail.com>
2013-10-24 14:54:47 +1000

    Now Area comes with a translation.

--- sources/Rewind/Area.hs
+++ sources/Rewind/Area.hs
@@ -27,6 +27,7 @@ import Data.Functor (Functor(..))
 import Data.Map (Map)
 import qualified Data.Map as Map
 import Data.Maybe (fromMaybe)
+import Data.Monoid (Monoid(..),(<>))
 
 data XY = XY
     { _x :: {-# UNPACK #-} !Int
@@ -34,21 +35,29 @@ data XY = XY
     } deriving (Eq,Ord,Read,Show)
 makeLenses ''XY
 
+instance Monoid XY where
+    mempty = XY 0 0
+    (XY ax ay) `mappend` (XY bx by) = XY (ax + bx) (ay + by)
+
 data Place =
     Rock
   | Floor
 
 data Area = Area
-    {   _parent :: !(XY → Place)
+    {   _translation :: {-# UNPACK #-} !XY
+    ,   _parent :: !(XY → Place)
     ,   _places :: !(Map XY Place)
     }
 makeLenses ''Area
+
+translate :: XY → Area → Area
+translate xy = translation %~ (<> xy)
 
 type instance Index Area = XY
 type instance IxValue Area = Place
 
 instance (Contravariant f, Functor f) => Contains f Area where
-    contains = containsTest (\xy area → Map.member xy (area ^. places))
+    contains = containsTest (\xy area → Map.member (area ^. translation <> xy) (area ^. places))
 
 instance At Area where
     at xy f area =
@@ -56,8 +65,9 @@ instance At Area where
         <&>
         maybe (maybe area insert is_member) insert
       where
-        is_member = area ^. places ^. to (Map.lookup xy)
-        insert = (area &) . (places %~) . Map.insert xy
+        abs_xy = area ^. translation <> xy
+        is_member = area ^. places ^. to (Map.lookup abs_xy)
+        insert = (area &) . (places %~) . Map.insert abs_xy
 
 instance Functor f ⇒ Ixed f Area where
     ix xy f area =
@@ -67,4 +77,6 @@ instance Functor f ⇒ Ixed f Area where
                 (area ^. places ^. to (Map.lookup xy))
             )
         <&>
-        (area &) . (places %~) . Map.insert xy
+        (area &) . (places %~) . Map.insert abs_xy
+      where
+        abs_xy = area ^. translation <> xy
