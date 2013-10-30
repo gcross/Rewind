04e2992b2d914556969ecdd95f1ccd47b4dce608
Gregory Crosswhite <gcrosswhite@gmail.com>
2013-10-30 14:08:16 +1000

    Wrote a simple test that the xy2i function is order preserving.

---
+++ tests/tests.hs
@@ -0,0 +1,70 @@
+-- Language extensions {{{
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE UnicodeSyntax #-}
+-- }}}
+
+-- Imports {{{
+import Control.Applicative ((<$>),(<*>))
+import Control.Monad (msum)
+
+import Control.Lens ((^.))
+
+import Test.Framework (defaultMain,testGroup)
+import Test.Framework.Providers.SmallCheck (testProperty)
+import Test.SmallCheck (Property(..))
+import Test.SmallCheck.Series (Serial(..),Series,getPositive)
+
+import Rewind.Area
+-- }}}
+
+-- Types {{{
+
+data BoundsAndXY = BoundsAndXY Bounds XY deriving (Eq,Ord,Read,Show)
+
+data BoundsAndXY2 = BoundsAndXY2 Bounds XY XY deriving (Eq,Ord,Read,Show)
+
+-- }}}
+
+-- Instances {{{
+
+instance Monad m ⇒ Serial m Bounds where -- {{{
+    series = Bounds <$> (getPositive <$> series) <*> (getPositive <$> series)
+-- }}}
+
+instance Monad m ⇒ Serial m BoundsAndXY where -- {{{
+    series = do
+        bounds ← series
+        BoundsAndXY <$> return bounds <*> xyForBounds bounds
+-- }}}
+
+instance Monad m ⇒ Serial m BoundsAndXY2 where -- {{{
+    series = do
+        bounds ← series
+        BoundsAndXY2 <$> return bounds <*> xyForBounds bounds <*> xyForBounds bounds
+-- }}}
+
+-- }}}
+
+-- Functions {{{
+
+xyForBounds :: Bounds → Series m XY -- {{{
+xyForBounds (Bounds width height) =
+    XY <$> (msum . map return $ [0..width-1]) <*> (msum . map return $ [0..height-1])
+-- }}}
+
+-- }}}
+
+main = defaultMain -- {{{
+    [testGroup "Rewind.Area" $ -- {{{
+        [testGroup "xy ↔ i" $ -- {{{
+            [testProperty "order preserving" $ \(BoundsAndXY2 bounds xy1 xy2) → -- {{{
+                xy1 `compare` xy2 == xy2i bounds xy1 `compare` xy2i bounds xy2
+             -- }}}
+            ]
+         -- }}}
+        ]
+     -- }}}
+    ]
+-- }}}
