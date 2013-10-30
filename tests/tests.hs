f4173dec197810822328d9a621b67978d693a2d9
Gregory Crosswhite <gcrosswhite@gmail.com>
2013-10-30 17:20:59 +1000

    Wrote testing infrastructure + a simple set of getting Contains.

--- tests/tests.hs
+++ tests/tests.hs
@@ -2,24 +2,35 @@
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TupleSections #-}
 {-# LANGUAGE UnicodeSyntax #-}
 -- }}}
 
 -- Imports {{{
 import Control.Applicative ((<$>),(<*>))
 import Control.Monad (msum)
+import Control.Lens ((^.),contains)
 
-import Control.Lens ((^.))
+import Data.IntMap (IntMap)
+import qualified Data.IntMap as IntMap
+import Data.Maybe (mapMaybe)
 
 import Test.Framework (defaultMain,testGroup)
-import Test.Framework.Providers.SmallCheck (testProperty)
+import qualified Test.Framework.Providers.QuickCheck2 as Quick
+import qualified Test.Framework.Providers.SmallCheck as Small
+import Test.QuickCheck.Arbitrary (Arbitrary(..),arbitraryBoundedEnum)
+import Test.QuickCheck.Gen (choose,vectorOf)
 import Test.SmallCheck (Property(..))
 import Test.SmallCheck.Series (Serial(..),Series,getPositive)
+
+import Text.Printf (printf)
 
 import Rewind.Area
 -- }}}
 
 -- Types {{{
+
+data AreaAndXY = AreaAndXY Area XY
 
 data BoundsAndIndex = BoundsAndIndex Bounds Int deriving (Eq,Ord,Read,Show)
 
@@ -30,6 +41,41 @@ data BoundsAndXY2 = BoundsAndXY2 Bounds XY XY deriving (Eq,Ord,Read,Show)
 -- }}}
 
 -- Instances {{{
+
+-- Arbitrary {{{
+
+instance Arbitrary Area where -- {{{
+    arbitrary = do
+        bounds ← arbitrary
+        parent ← arbitrary
+        places ←
+            IntMap.fromAscList
+            .
+            mapMaybe (\(i,maybe_place) → (i,) <$> maybe_place)
+            .
+            zip [0..]
+            <$>
+            vectorOf (numberOfPlaces bounds) arbitrary
+        return $ Area bounds parent places
+-- }}}
+
+instance Arbitrary AreaAndXY where -- {{{
+    arbitrary = do
+        area ← arbitrary
+        x ← choose (0,area^.width-1)
+        y ← choose (0,area^.height-1)
+        return $ AreaAndXY area (XY x y)
+-- }}}
+
+instance Arbitrary Bounds where -- {{{
+    arbitrary = Bounds <$> choose (1,10) <*> choose (1,10)
+-- }}}
+
+instance Arbitrary Place where arbitrary = arbitraryBoundedEnum
+
+-- }}}
+
+-- Serial {{{
 
 instance Monad m ⇒ Serial m Bounds where -- {{{
     series = Bounds <$> (getPositive <$> series) <*> (getPositive <$> series)
@@ -55,6 +101,16 @@ instance Monad m ⇒ Serial m BoundsAndXY2 where -- {{{
 
 -- }}}
 
+instance Show AreaAndXY where -- {{{
+    show (AreaAndXY area xy) =
+        printf "AreaAndXY (Area %s _ %s) %s"
+            (show (area^.bounds))
+            (show (area^.places))
+            (show xy)
+-- }}}
+
+-- }}}
+
 -- Functions {{{
 
 xyForBounds :: Bounds → Series m XY -- {{{
@@ -67,15 +123,26 @@ xyForBounds (Bounds width height) =
 main = defaultMain -- {{{
     [testGroup "Rewind.Area" $ -- {{{
         [testGroup "xy ↔ i" $ -- {{{
-            [testProperty "order preserving" $ \(BoundsAndXY2 bounds xy1 xy2) → -- {{{
+            [Small.testProperty "order preserving" $ \(BoundsAndXY2 bounds xy1 xy2) → -- {{{
                 xy1 `compare` xy2 == xy2i bounds xy1 `compare` xy2i bounds xy2
              -- }}}
             ,testGroup "xy_i is an isomorphism" -- {{{
-                [testProperty "forward direction" $ \(BoundsAndXY bounds xy) → -- {{{
+                [Small.testProperty "forward direction" $ \(BoundsAndXY bounds xy) → -- {{{
                     (i2xy bounds . xy2i bounds) xy == xy
                  -- }}}
-                ,testProperty "backward direction" $ \(BoundsAndIndex bounds i) → -- {{{
+                ,Small.testProperty "backward direction" $ \(BoundsAndIndex bounds i) → -- {{{
                     (xy2i bounds . i2xy bounds) i == i
+                 -- }}}
+                ]
+             -- }}}
+            ]
+         -- }}}
+        ,testGroup "Area instances" $ -- {{{
+            [testGroup "Contains" $ -- {{{
+                [Quick.testProperty "get" $ \(AreaAndXY area xy) → -- {{{
+                    area ^. contains xy
+                    ==
+                    IntMap.member (xy2i (area^.bounds) xy) (area^.places)
                  -- }}}
                 ]
              -- }}}
